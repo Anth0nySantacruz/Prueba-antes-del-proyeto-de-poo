202c40ac24e9ca37ee4f11fbedab29ae2e6bc938
mega_ <mega_@LAPTOP-J4LEG9CE.local>
2023-11-10 18:16:13 -0500

    primer commit de prueba

---
+++ src/main/java/ec/edu/espol/pruebagithub/PruebaGitHub.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.pruebagithub;
+
+/**
+ *
+ * @author mega_
+ */
+public class PruebaGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hola");
+    }
+    
+}
