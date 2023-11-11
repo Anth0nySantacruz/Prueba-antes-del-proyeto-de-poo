ac0dae855b3e1622488496ec682ab5c99c67c111
Anthonys <aasantac@espol.edu.ec>
2023-11-10 19:28:54 -0500

    cambio de clase y modificacion del main

--- src/main/java/ec/edu/espol/pruebagithub/ClaseNueva.java
+++ src/main/java/ec/edu/espol/pruebagithub/ClaseNueva.java
@@ -1,3 +1,5 @@
+package ec.edu.espol.pruebagithub;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +10,5 @@
  * @author mega_
  */
 public class ClaseNueva {
-    
+    private int val;
 }
--- src/main/java/ec/edu/espol/pruebagithub/PruebaGitHub.java
+++ src/main/java/ec/edu/espol/pruebagithub/PruebaGitHub.java
@@ -13,7 +13,7 @@ public class PruebaGitHub {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Holiii");
-        System.out.println("ho");
+        int vol;
     }
     
 }
